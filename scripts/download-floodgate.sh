4bca8b181922e598e8251a8bf9284a0a4177c146
nakabonne <ryo@nakao.dev>
2023-10-28 21:52:46 +0900

    Add script to download kifu

---
+++ scripts/download-floodgate.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+function usage() {
+    echo "Usage: $0 <year-1> <year-2>..."
+    echo "Example: $0 2021 2022 2023"
+    echo "-h | --help       Show this help message"
+}
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -h | --help )
+            usage
+            exit
+            ;;
+    esac
+    shift
+done
+
+cd kifu
+for var in "$@"
+do
+    echo "Connecting to http://wdoor.c.u-tokyo.ac.jp/shogi/x/wdoor$var.7z"
+    wget http://wdoor.c.u-tokyo.ac.jp/shogi/x/wdoor$var.7z
+    7z x wdoor$var.7z
+done
